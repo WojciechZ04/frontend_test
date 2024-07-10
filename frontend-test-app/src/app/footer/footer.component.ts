82c55067274b252e4e34ed951ddc36ce983f1e36
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-07-10 18:31:18 +0200

    footer update: add CSS IS AWESOME with animation and create experimental button

--- frontend-test-app/src/app/footer/footer.component.ts
+++ frontend-test-app/src/app/footer/footer.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  isViewportVisible: boolean = false;
 
+  toggleViewport(): void {
+    this.isViewportVisible = !this.isViewportVisible;
+  }
+
+  reset(): void {
+    window.location.reload(); // Refreshes the page
+  }
 }
