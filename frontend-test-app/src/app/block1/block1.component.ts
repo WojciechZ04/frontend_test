deb8c3c2fb1e80dd5fe9f7c6fabac4f3888a7023
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-07-10 21:05:00 +0200

    update blocks positioning and styling

--- frontend-test-app/src/app/block1/block1.component.ts
+++ frontend-test-app/src/app/block1/block1.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-block1',
   standalone: true,
-  imports: [],
+  imports: [MatRadioModule],
   templateUrl: './block1.component.html',
   styleUrl: './block1.component.scss'
 })
